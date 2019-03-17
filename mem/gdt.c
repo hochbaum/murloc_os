69422eed261e46e32d675a0849afa141d6ed1902
Linus Hochbaum <contact@lhochbaum.de>
2019-03-17 14:26:32 +0100

    Fix linker and set the GDT.

---
+++ mem/gdt.c
@@ -0,0 +1,5 @@
+//
+// Created by Linus Hochbaum on 17.03.19.
+//
+
+#include "gdt.h"
---
+++ mem/gdt.h
@@ -0,0 +1,8 @@
+//
+// Created by Linus Hochbaum on 17.03.19.
+//
+
+#ifndef MURLOC_OS_GDT_H
+#define MURLOC_OS_GDT_H
+
+#endif //MURLOC_OS_GDT_H
